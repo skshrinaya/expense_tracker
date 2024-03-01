ad3c3b43f29f3aec8aaeeaf3dce657cf49e2b2b9
skshrinaya <shrinaya.sk2022cce@sece.ac.in>
2024-03-01 10:46:46 +0530

    endpoint

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const app = express()
 const static = express.static('static');
 app.use("/",static);
 
-// app.get('/', function (req, res) {
-//  res.send("hello World")})
+app.get("/hi",(req,res)=>{
+    console.log(req.query)
+    //res.send("hello World")
+    //http://localhost:4000/hi?phone=12
+    /**
+     * host: localhost/127.0.0.1
+     * port: 4000
+     * path:/hi
+     * ?key=value
+     */
+    res.json({
+        form:req.query,
+        //phone:req.query.phone*10
+    });
+});
+//  })
 
 app.listen(4000,()=>{
     console.log("App running");
