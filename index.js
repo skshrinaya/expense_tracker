b7dcf270a74327b86397ad9a8f42b4093046b510
skshrinaya <shrinaya.sk2022cce@sece.ac.in>
2024-03-01 14:46:38 +0530

    thirdparty

--- index.js
+++ index.js
@@ -8,28 +8,42 @@ app.use(bodyParser.urlencoded({extended:true}));
 const static = express.static('static');
 app.use("/",static);
 
+app.get("/hi",(req,res)=>{
+    const {amount}=req.query;
+    res.json({
+        firstName:req.query.name,price:amount,
+    ...req.query})
+});
+
+app.get("/todos",async (req,res)=>{
+//   fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then((response)=>response.json()).
+//   then((json)=>res.json(json));
+
+  //res.json(todos);
+
+   const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+   const todos = await response.json()
+   res.json(todos)
+});
+
+app.get("/todos/:id",async(req,res)=>{
+    const {id:todoId}=req.params;
+    res.json({todoId})
+
+});
+
+
 app.post("/hi",(req,res)=>{
     console.log(req.body)
-    //res.send("hello World")
-    //http://localhost:4000/hi?phone=12
-    /**
-     * host: localhost/127.0.0.1
-     * port: 4000
-     * path:/hi
-     * ?key=value
-     */
     res.json({
         name:req.body.name,
         description:req.body.description,
         amount:req.body.amount,
         date:req.body.date,
-
-
-        //form:req.query,
-        //phone:req.query.phone*10
     });
-});
-//  })
+    
+ })
 
 app.listen(4000,()=>{
     console.log("App running");
